1e5192e6d3a354a2246082efcc2ae663c1b377eb
cjsrn1940 <cjsrn1940@gmail.com>
2021-06-30 23:32:10 +0900

    6/30 Q7~10 추가

--- Practice/Practice05.sql
+++ Practice/Practice05.sql
@@ -108,3 +108,107 @@ from employees e, departments d
 where e.department_id = d.department_id
 and e.hire_date = (select  max(hire_date)
                     from employees);
+         
+--Q7
+--Q7
+select  e.employee_id,
+        e.first_name,
+        e.last_name,
+        e.salary,
+        aSlt.aSalary,
+        j.job_title
+from    employees e, jobs j,
+        (
+            select  department_id
+            from    (
+                        select  avg(salary) aSalary,
+                                department_id
+                        from employees 
+                        group by department_id
+                    ) aSel,
+                    (
+                        select  max(aSelect.aSalary) mSalary
+                        from    (select  avg(salary) aSalary,
+                                         department_id
+                                from employees
+                                group by department_id) aSelect
+                    ) mSel
+            where aSel.aSalary = mSel.mSalary
+        ) endS,
+        (
+            select  avg(salary) aSalary,
+                    department_id
+            from employees 
+            group by department_id
+        ) aSlt
+where e.job_id = j.job_id
+and e.department_id = ends.department_id
+and e.department_id = aslt.department_id;
+
+--Q8
+select  d.department_name
+from    departments d,
+        (
+            select  department_id
+            from    (
+                        select  avg(salary) aSalary,
+                                department_id
+                        from employees 
+                        group by department_id
+                    ) aSel,
+                    (
+                        select  max(aSelect.aSalary) mSalary
+                        from    (select  avg(salary) aSalary,
+                                         department_id
+                                from employees
+                                group by department_id) aSelect
+                    ) mSel
+            where aSel.aSalary = mSel.mSalary
+        ) endS
+where d.department_id = endS.department_id;
+
+
+--Q9
+select  rAvg.region_name
+from    (
+            select  r.region_name, avg(e.salary) aSalary
+            from    regions r, countries c, locations l, departments d, employees e
+            where e.department_id = d.department_id
+            and d.location_id = l.location_id
+            and l.country_id = c.country_id
+            and c.region_id = r.region_id
+            group by r.region_name
+        ) rAvg,
+        (
+            select  max(rSalary.aSalary) mSalary
+            from    (
+                        select  r.region_name, avg(e.salary) aSalary
+                        from    regions r, countries c, locations l, departments d, employees e
+                        where e.department_id = d.department_id
+                        and d.location_id = l.location_id
+                        and l.country_id = c.country_id
+                        and c.region_id = r.region_id
+                        group by r.region_name
+                    ) rSalary
+        ) rMax
+where rAvg.aSalary = rMax.mSalary;
+
+
+--Q10
+select  job_title
+from    (
+            select  j.job_title, avg(e.salary) aSalary
+            from    jobs j, employees e
+            where e.job_id = j.job_id
+            group by j.job_title
+        ) jAvg,
+        (
+            select  max(jSalary.aSalary) mSalary
+            from    (
+                        select  j.job_title, avg(e.salary) aSalary
+                        from    jobs j, employees e
+                        where e.job_id = j.job_id
+                        group by j.job_title
+                    ) jSalary
+        ) jMax
+where jAvg.aSalary = jMax.mSalary;
